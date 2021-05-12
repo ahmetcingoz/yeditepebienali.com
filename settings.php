8acbb435b3d835601b3d5392d62fcf75f002b7ec
ahmetcingoz <ahmetarifcingoz@gmail.com>
2021-05-12 16:05:02 +0300

    image update

--- settings.php
+++ settings.php
@@ -4,7 +4,7 @@
 
 	$title = 'Yeditepe Bienali';
 
-	$rename_path = 'images/articles/sergiler/2018/grup-sergileri/sule-i-tiryakiart/';
+	$rename_path = 'images/articles/sergiler/2018/karma-sergiler/bienal-karma-sergisi/';
 
 	/* SETTINGS */
 
