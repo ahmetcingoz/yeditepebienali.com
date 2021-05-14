d1ee3f6521ddbc0cd30006fc8d3c9726abbadebc
ahmetcingoz <ahmetarifcingoz@gmail.com>
2021-05-14 23:01:41 +0300

    mekanlar update

--- include/pages/mekanlar.php
+++ include/pages/mekanlar.php
@@ -23,119 +23,118 @@
 			<div class="col-xs-12">
 				<!-- article holder -->
 				<div class="article-holder">
-					<!-- article -->
-					<article class="article">
-						<div class="image wow fadeInLeft" data-wow-delay="0.6s">
-							<img src="http://placehold.it/570x440" alt="image description">
-						</div>
-						<div class="text wow fadeInRight" data-wow-delay="0.6s">
-							<strong class="title">by Leonard, 1 week ago</strong>
-							<div class="txt">
-								<h2>We Do The Best Design</h2>
-								<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa  officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem.</p>
-								<a class="more" href="blog-detail.html">
-									<span>CONTINUE</span>
-									<i class="icon-right-thin"></i>
-								</a>
-							</div>
-						</div>
-					</article>
-					<!-- article -->
-					<article class="article">
-						<div class="text wow fadeInLeft" data-wow-delay="0.6s">
-							<strong class="title">by Leonard, 1 week ago</strong>
-							<div class="txt">
-								<h2>Friendly For All Devices</h2>
-								<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa  officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem.</p>
-								<a class="more" href="blog-detail.html">
-									<span>CONTINUE</span>
-									<i class="icon-right-thin"></i>
-								</a>
-							</div>
-						</div>
-						<div class="image wow fadeInRight" data-wow-delay="0.6s">
-							<img src="http://placehold.it/570x440" alt="image description">
-						</div>
-					</article>
-					<!-- article -->
-					<article class="article">
-						<div class="image wow fadeInLeft" data-wow-delay="0.6s">
-							<img src="http://placehold.it/570x440" alt="image description">
-						</div>
-						<div class="text wow fadeInRight" data-wow-delay="0.6s">
-							<strong class="title">by Leonard, 1 week ago</strong>
-							<div class="txt">
-								<h2>We Do The Best Design</h2>
-								<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa  officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem.</p>
-								<a class="more" href="blog-detail.html">
-									<span>CONTINUE</span>
-									<i class="icon-right-thin"></i>
-								</a>
-							</div>
-						</div>
-					</article>
-					<!-- article -->
-					<article class="article">
-						<div class="text wow fadeInLeft" data-wow-delay="0.6s">
-							<strong class="title">by Leonard, 1 week ago</strong>
-							<div class="txt">
-								<h2>Friendly For All Devices</h2>
-								<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa  officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem.</p>
-								<a class="more" href="blog-detail.html">
-									<span>CONTINUE</span>
-									<i class="icon-right-thin"></i>
-								</a>
-							</div>
-						</div>
-						<div class="image wow fadeInRight" data-wow-delay="0.6s">
-							<img src="http://placehold.it/570x440" alt="image description">
-						</div>
-					</article>
-					<!-- article -->
-					<article class="article">
-						<div class="image wow fadeInLeft" data-wow-delay="0.6s">
-							<img src="http://placehold.it/570x440" alt="image description">
-						</div>
-						<div class="text wow fadeInRight" data-wow-delay="0.6s">
-							<strong class="title">by Leonard, 1 week ago</strong>
-							<div class="txt">
-								<h2>We Do The Best Design</h2>
-								<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa  officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem.</p>
-								<a class="more" href="blog-detail.html">
-									<span>CONTINUE</span>
-									<i class="icon-right-thin"></i>
-								</a>
-							</div>
-						</div>
-					</article>
-					<!-- article -->
-					<article class="article">
-						<div class="text wow fadeInLeft" data-wow-delay="0.6s">
-							<strong class="title">by Leonard, 1 week ago</strong>
-							<div class="txt">
-								<h2>Friendly For All Devices</h2>
-								<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa  officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem.</p>
-								<a class="more" href="blog-detail.html">
-									<span>CONTINUE</span>
-									<i class="icon-right-thin"></i>
-								</a>
-							</div>
-						</div>
-						<div class="image wow fadeInRight" data-wow-delay="0.6s">
-							<img src="http://placehold.it/570x440" alt="image description">
-						</div>
-					</article>
-				</div>
-				<!-- paginations -->
-				<div class="paginations">
-					<ul>
-						<li class="active"><a href="#">1</a></li>
-						<li><a href="#">2</a></li>
-						<li><a href="#">3</a></li>
-						<li><a href="#"><i class="icon-right-open-mini"></i></a></li>
-					</ul>
-				</div>
+					
+					<?php 
+					
+						$odd = 0;
+					
+						function _odd_even($number){
+							
+							if ($number % 2 == 0) {
+								
+								return true;
+								
+							} 
+							
+						}					
+					
+						$artists = $_SERVER['DOCUMENT_ROOT'] . '/json/category/venues.json';
+
+						$artists = _decode_json($artists);
+
+						foreach ($artists as $category_key => $category_value) {
+
+							foreach ($category_value as $date_key => $date_value) {
+								
+								foreach ($date_value as $venues_key => $venues_value) { 
+								
+								if (_odd_even($odd++) == true) { ?>
+
+								<article class="article">
+									<div class="image wow fadeInLeft" data-wow-delay="0.6s">
+										<img src="http://placehold.it/570x440" alt="image description">
+									</div>
+									<div class="text wow fadeInRight" data-wow-delay="0.6s">
+										<!--<strong class="title"><?php echo _translate('venues', $venues_key) ?></strong>-->
+										<div class="txt">
+											<h2><?php echo _translate('venues', $venues_key) ?></h2>
+											<p>
+												<?php 
+									
+												$word_count = $venues_value[$_SESSION['language']];
+												
+												$word_count = explode(' ', $word_count);
+																					   
+												for ($i = 0; $i <= 24; $i++) {
+													
+													echo $word_count[$i] . ' ';
+													
+												}
+												 
+												echo '...';
+																					   
+												?>
+											
+											</p>
+											<a class="more" href="<?php echo _seo(_translate('venues', $venues_key)) ?>">
+												<span><?php echo _translate('word', 'devamı') ?></span>
+												<i class="icon-right-thin"></i>
+											</a>
+										</div>
+									</div>
+								</article>					
+					
+								<?php } else { ?>
+									
+								<article class="article">
+									<div class="text wow fadeInLeft" data-wow-delay="0.6s">
+										<!--<strong class="title"><?php echo _translate('venues', $venues_key) ?></strong>-->
+										<div class="txt">
+											<h2><?php echo _translate('venues', $venues_key) ?></h2>
+											
+												<p>
+												<?php 
+									
+												$word_count = $venues_value[$_SESSION['language']];
+												
+												$word_count = explode(' ', $word_count);
+																					   
+												for ($i = 0; $i <= 24; $i++) {
+													
+													echo $word_count[$i] . ' ';
+													
+												}
+												 
+												echo '...';
+																					   
+												?>
+											
+											</p>
+											<a class="more" href="<?php echo _seo(_translate('venues', $venues_key)) ?>">
+												<span><?php echo _translate('word', 'devamı') ?></span>
+												<i class="icon-right-thin"></i>
+											</a>
+										</div>
+									</div>
+									<div class="image wow fadeInRight" data-wow-delay="0.6s">
+										<img src="http://placehold.it/570x440" alt="image description">
+									</div>
+								</article>										
+									
+								<?php } ?>
+
+							<?php }
+
+							}
+
+						}
+
+					?>					
+					
 			</div>
 		</div>
 	</div>
 </main>
+	
+					<!-- article -->
+	
